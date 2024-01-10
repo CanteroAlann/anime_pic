cece57f85587c0584160b5de4757d961a5b4f229
alan <acantero@fi.uba.ar>
2024-01-10 15:10:10 -0300

    enviroment for test working

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,8 @@ mongoose.connect(config.MONGODB_URI)
 app.use(express.json());
 app.use(middleware.requestLogger)
 app.use(cors())
-app.use('/api/images', imageRouter);
+app.use('/api/images', imageRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
